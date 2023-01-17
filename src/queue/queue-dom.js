454962d0e3be260c2e1f754cc935cd3a496e8cf9
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-01-17 17:21:40 +0100

    Jasmine tasks done, all green. DOM.js files only halfway completed.

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -7,14 +7,30 @@ const warningBottomQueue = document.querySelector(
 const addQueue = document.querySelector('.btn-add-queue');
 const dequeue = document.querySelector('.btn-take-dequeue');
 
-const queue = new Queue();
+const newQueue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = newQueue.display().length;
+  let size = newQueue.MAX_SIZE - length;
+  newQueue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
